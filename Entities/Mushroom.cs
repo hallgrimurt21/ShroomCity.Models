9c22bc0f810cd85f4465052ce2860b0d2cfcd351
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 23:07:49 +0000

    Update Name property in Mushroom class

--- Entities/Mushroom.cs
+++ Entities/Mushroom.cs
@@ -3,7 +3,7 @@ namespace ShroomCity.Models.Entities;
 public class Mushroom
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public required string Name { get; set; }
     public string? Description { get; set; }
 
     // Navigation properties
