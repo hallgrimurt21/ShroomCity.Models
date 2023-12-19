70ac90d0b8574e5ac02842d6b9d785469d28df6d
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 23:20:57 +0000

    Add properties to MushroomDetailsDto class

--- Dtos/MushroomDetailsDto.cs
+++ Dtos/MushroomDetailsDto.cs
@@ -2,4 +2,8 @@ namespace ShroomCity.Models.Dtos;
 
 public class MushroomDetailsDto
 {
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public List<AttributeDto> Attributes { get; set; } = new List<AttributeDto>();
 }
