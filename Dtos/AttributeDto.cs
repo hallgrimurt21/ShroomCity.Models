50e29c9fba7bd223255c2e296bccf2b5e74b7d35
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 22:43:16 +0000

    Add 'required' keyword to Value and Type properties in AttributeDto

--- Dtos/AttributeDto.cs
+++ Dtos/AttributeDto.cs
@@ -3,8 +3,8 @@ namespace ShroomCity.Models.Dtos;
 public class AttributeDto
 {
     public int Id { get; set; }
-    public string Value { get; set; }
-    public string Type { get; set; }
+    public required string Value { get; set; }
+    public required string Type { get; set; }
     public string? RegisteredBy { get; set; }
     public DateTime? RegistrationDate { get; set; }
 }
