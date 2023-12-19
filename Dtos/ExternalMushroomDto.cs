01e223b497a0254079938cfd11c8a5a5b5a8062c
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 23:20:53 +0000

    Add properties to ExternalMushroomDto

--- Dtos/ExternalMushroomDto.cs
+++ Dtos/ExternalMushroomDto.cs
@@ -1,5 +1,14 @@
 namespace ShroomCity.Models.Dtos;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 public class ExternalMushroomDto
 {
+    [JsonPropertyName("_id")]
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required List<string> Colors { get; set; }
+    public required List<string> Shapes { get; set; }
+    public required List<string> Surfaces { get; set; }
 }
