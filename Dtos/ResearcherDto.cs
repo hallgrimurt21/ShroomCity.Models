de6310203aeb1885cf97b3fe4881d015b4920968
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 23:05:55 +0000

    Add properties to ResearcherDto class

--- Dtos/ResearcherDto.cs
+++ Dtos/ResearcherDto.cs
@@ -2,4 +2,9 @@ namespace ShroomCity.Models.Dtos;
 
 public class ResearcherDto
 {
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string EmailAddress { get; set; }
+    public string? Bio { get; set; }
+    public List<MushroomDto>? AssociatedMushrooms { get; set; }
 }
