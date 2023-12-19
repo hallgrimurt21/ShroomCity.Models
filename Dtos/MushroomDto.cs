490d0f8015e347c5f0681a2cfda87778c9be8eda
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 23:07:40 +0000

    Add properties to MushroomDto class

--- Dtos/MushroomDto.cs
+++ Dtos/MushroomDto.cs
@@ -1,5 +1,10 @@
 namespace ShroomCity.Models.Dtos;
+using System.Text.Json.Serialization;
 
 public class MushroomDto
 {
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
 }
