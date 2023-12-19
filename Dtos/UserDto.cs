60cf7729359ee00c1d49d2ccfedfe1ff5296fe05
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 22:43:31 +0000

    Add properties to UserDto class

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -2,4 +2,10 @@ namespace ShroomCity.Models.Dtos;
 
 public class UserDto
 {
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string EmailAddress { get; set; }
+    public string? Bio { get; set; }
+    public required List<string> Permissions { get; set; }
+    public int TokenId { get; set; }
 }
