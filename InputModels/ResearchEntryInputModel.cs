723e0aad17e087e2cca72a5bd6ddea576e83cfd0
hallgrimurt21 <hallgrimurth@gmail.com>
2023-12-20 21:27:26 +0000

    Add Entry class to ResearchEntryInputModel

--- InputModels/ResearchEntryInputModel.cs
+++ InputModels/ResearchEntryInputModel.cs
@@ -1,8 +1,13 @@
 namespace ShroomCity.Models.InputModels;
 using System.Collections.Generic;
 
+public class Entry
+{
+    public required string Key { get; set; }
+    public required string Value { get; set; }
+}
 
 public class ResearchEntryInputModel
 {
-    public List<KeyValuePair<string?, string?>> Entries { get; set; }
+    public List<Entry>? Entries { get; set; }
 }
