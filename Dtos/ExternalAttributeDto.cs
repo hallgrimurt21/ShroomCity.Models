1a0285bcd90fad86dfc39da5558506e263cfb992
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-20 02:35:22 +0000

    Add properties to ExternalAttributeDto

--- Dtos/ExternalAttributeDto.cs
+++ Dtos/ExternalAttributeDto.cs
@@ -1,5 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace ShroomCity.Models.Dtos;
 
 public class ExternalAttributeDto
 {
+    [JsonPropertyName("_id")]
+    public required string Id { get; set; }
+    public required string Name { get; set; }
 }
