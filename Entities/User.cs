51e77aedc615e6892fc58d61961c566d3b1bbed0
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-20 00:44:37 +0000

    Fix wrong version uses

--- Entities/User.cs
+++ Entities/User.cs
@@ -3,11 +3,17 @@ namespace ShroomCity.Models.Entities;
 public class User
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string Name { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public string? Bio { get; set; }
-    public required string HashedPassword { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string HashedPassword { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public DateTime RegisterationDate { get; set; }
-    public required string EmailAddress { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string EmailAddress { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     // Navigation properties
     public ICollection<Role> Roles { get; set; } = new List<Role>();
--- InputModels/LoginInputModel.cs
+++ InputModels/LoginInputModel.cs
@@ -5,9 +5,13 @@ public class LoginInputModel
 {
     [Required]
     [EmailAddress]
-    public required string EmailAddress { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string EmailAddress { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     [Required]
     [MinLength(6)]
-    public required string Password { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string Password { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
