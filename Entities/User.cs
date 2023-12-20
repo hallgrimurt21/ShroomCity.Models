dd1ad5590e4e910b778a1e8681facdec75f0d3c1
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-20 11:55:24 +0000

    Update User class properties to use required attribute

--- Entities/User.cs
+++ Entities/User.cs
@@ -3,17 +3,11 @@ namespace ShroomCity.Models.Entities;
 public class User
 {
     public int Id { get; set; }
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public string Name { get; set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public required string Name { get; set; }
     public string? Bio { get; set; }
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public string HashedPassword { get; set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public required string HashedPassword { get; set; }
     public DateTime RegisterationDate { get; set; }
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public string EmailAddress { get; set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public required string EmailAddress { get; set; }
 
     // Navigation properties
     public ICollection<Role> Roles { get; set; } = new List<Role>();
