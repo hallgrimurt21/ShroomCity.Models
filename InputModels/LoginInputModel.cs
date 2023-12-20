78bfc1cd390795db44a51d35db032407627ebe10
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-20 00:16:34 +0000

    Add validation attributes to LoginInputModel properties

--- InputModels/LoginInputModel.cs
+++ InputModels/LoginInputModel.cs
@@ -1,5 +1,13 @@
 namespace ShroomCity.Models.InputModels;
+using System.ComponentModel.DataAnnotations;
 
 public class LoginInputModel
 {
+    [Required]
+    [EmailAddress]
+    public required string EmailAddress { get; set; }
+
+    [Required]
+    [MinLength(6)]
+    public required string Password { get; set; }
 }
