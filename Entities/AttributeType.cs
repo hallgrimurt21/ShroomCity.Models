4718809c03df5df8b538124ea273290be5725bc9
Hallgrímur <89984159+hallgrimurt21@users.noreply.github.com>
2023-12-30 16:08:34 +0000

    Update AttributeType.cs to make 'Type' property required

--- Entities/AttributeType.cs
+++ Entities/AttributeType.cs
@@ -3,7 +3,7 @@ namespace ShroomCity.Models.Entities;
 public class AttributeType
 {
     public int Id { get; set; }
-    public string? Type { get; set; }
+    public required string Type { get; set; }
 
     // Navigation properties
     public Attribute? Attribute { get; set; } // Navigation property
