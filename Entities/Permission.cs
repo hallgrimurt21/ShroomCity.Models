dd3b7a4b4ebd5a93c4ba609224a92c8c34871bfc
Hallgrímur <89984159+hallgrimurt21@users.noreply.github.com>
2023-12-30 12:49:27 +0000

    Fix nullable reference error in Permission.cs

--- Entities/Permission.cs
+++ Entities/Permission.cs
@@ -5,7 +5,7 @@ public class Permission
 #pragma warning restore CA1711 // Identifiers should not have incorrect suffix
 {
     public int Id { get; set; }
-    public string? Code { get; set; }
+    public required string Code { get; set; }
     public string? Description { get; set; }
 
     // Navigation properties
