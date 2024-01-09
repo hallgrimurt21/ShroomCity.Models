6b015cb6b3df6ca9b1925015413363e6256dcfc0
hallgrimurt21 <hallgrimurjth@gmail.com>
2024-01-09 20:57:52 +0000

    Add ToString() method and JsonPropertyName attribute to Envelope class

--- Envelope.cs
+++ Envelope.cs
@@ -1,9 +1,13 @@
 namespace ShroomCity.Models;
 
+using System.Text.Json.Serialization;
+
 public class Envelope<T> where T : class
 {
+    public override string ToString() => $"PageSize: {this.PageSize}, PageNumber: {this.PageNumber}, TotalPages: {this.TotalPages}, Items: {this.Items}";
     public int PageSize { get; set; }
     public int PageNumber { get; set; }
     public int TotalPages { get; set; }
+    [JsonPropertyName("data")]
     public IEnumerable<T> Items { get; set; } = null!;
 }
