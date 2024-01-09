b360ad4d3037a29520c780651c3cb35f7061480e
hallgrimurt21 <hallgrimurjth@gmail.com>
2024-01-09 20:59:06 +0000

    Add TokenIdClaimType to ClaimTypeConstants

--- Constants/ClaimTypeConstants.cs
+++ Constants/ClaimTypeConstants.cs
@@ -3,4 +3,5 @@ namespace ShroomCity.Models.Constants;
 public static class ClaimTypeConstants
 {
     public const string PermissionClaimType = "permissions";
+    public const string TokenIdClaimType = "tokenId";
 }
