a4bdef4e7f26e3234508d52e2603c148060fd263
hallgrimurt21 <hallgrimurjth@gmail.com>
2024-01-11 20:36:43 +0000

    Add AttributeTypeConstants

---
+++ Constants/AttributeTypeConstants.cs
@@ -0,0 +1,10 @@
+namespace ShroomCity.Models.Constants;
+
+public static class AttributeTypeConstants
+{
+    public const string Color = "Color";
+    public const string Shape = "Shape";
+    public const string Surface = "Surface";
+    public const string CapSize = "CapSize";
+    public const string StemSize = "StemSize";
+}
